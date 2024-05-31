15acdaa68c7c4fa293b8231cd5adf8153c39b845
maurixavi <mxavierarevalo@gmail.com>
2024-05-31 18:18:34 -0300

    Update cors

--- index.js
+++ index.js
@@ -12,7 +12,16 @@ const port = process.env.PORT || 4000;
 const jwtSecret = 'asdaf4554asd45asdxdggdsfk1'
 
 const whitelist = ['http://localhost:3000', 'https://mern-blog-client-nn6u.vercel.app/'];
-app.use(cors({ origin: whitelist }));
+app.use(cors({
+  credentials: true,
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 app.use(express.json());
 app.use(cookieParser());
 
