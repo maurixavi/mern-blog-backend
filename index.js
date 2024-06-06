f05551a5f41da60801d59b0c1089cb961f02e500
maurixavi <mxavierarevalo@gmail.com>
2024-06-06 02:52:36 -0300

    Add Upload Files management

--- index.js
+++ index.js
@@ -7,14 +7,21 @@ const Post = require('./models/Post');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+const fs = require('fs');
+const path = require('path');
+
 const app = express();
-const multer = require('multer')
-const uploadMiddleware = multer({dest: 'uploads/'})
-const fs = require('fs')
-
-
 const port = process.env.PORT || 4000;
-const jwtSecret = 'asdaf4554asd45asdxdggdsfk1'
+const jwtSecret = 'asdaf4554asd45asdxdggdsfk1';
+
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
+});
 
 const whitelist = ['http://localhost:3000', 'https://mern-blog-client-nn6u.vercel.app', 'https://mern-blog-client-torr.onrender.com'];
 app.use(cors({
@@ -27,19 +34,23 @@ app.use(cors({
     }
   }
 }));
-app.set("trust proxy",1);
+app.set("trust proxy", 1);
 app.use(express.json());
 app.use(cookieParser());
-app.use('/uploads', express.static(__dirname + '/uploads'))
 
-const saltRounds = 10; // Número de rondas de hashing para bcrypt
+app.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir: './uploads'
+}));
+
+const saltRounds = 10;
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-.then(() => console.log('Conectado a MongoDB'))
-.catch((err) => console.error('Error al conectar a MongoDB', err));
+  .then(() => console.log('Conectado a MongoDB'))
+  .catch((err) => console.error('Error al conectar a MongoDB', err));
 
 app.post('/register', async (req, res) => {
   const { username, password } = req.body;
@@ -51,9 +62,9 @@ app.post('/register', async (req, res) => {
     }
 
     const hashedPassword = await bcrypt.hash(password, saltRounds);
-    const userDoc = await User.create({ 
-      username, 
-      password: hashedPassword 
+    const userDoc = await User.create({
+      username,
+      password: hashedPassword
     });
     res.json(userDoc);
   } catch (err) {
@@ -82,7 +93,7 @@ app.post('/login', async (req, res) => {
     console.log('Login successful for:', username);
     res.cookie('token', token, {
       httpOnly: true,
-      secure: true, 
+      secure: true,
       sameSite: 'None'
     }).json({
       token: token,
@@ -107,40 +118,9 @@ app.post('/logout', (req, res) => {
   res.clearCookie('token').json({ message: 'Logged out' });
 });
 
-app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
-  const {originalname, path} = req.file
-  const parts = originalname.split('.')
-  const ext = parts[parts.length - 1]
-  const newPath = path+'.'+ext
-  fs.renameSync(path, newPath)
-
-  const {token} = req.cookies;
-  jwt.verify(token, jwtSecret, {}, async (err, info) => {
-    if (err) {
-      console.error('Token verification failed:', err);
-      return res.status(401).json({ error: 'Invalid token' });
-    }
-    const {title, summary, content } = req.body
-    const postDoc = await Post.create({ 
-      title, 
-      summary,
-      content, 
-      cover: `https://mern-blog-api-eight.vercel.app/uploads/${newPath}`,
-      // cover:newPath, 
-      author:info.id,
-    });
-    res.json(postDoc)
-  });
-})
-
-app.put('/post', uploadMiddleware.single('file'), async (req, res) => {
-  let newPath = null;
-  if (req.file) {
-    const { originalname, path } = req.file;
-    const parts = originalname.split('.');
-    const ext = parts[parts.length - 1];
-    newPath = path + '.' + ext;
-    fs.renameSync(path, newPath);
+app.post('/post', async (req, res) => {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
   }
 
   const { token } = req.cookies;
@@ -149,6 +129,35 @@ app.put('/post', uploadMiddleware.single('file'), async (req, res) => {
       console.error('Token verification failed:', err);
       return res.status(401).json({ error: 'Invalid token' });
     }
+
+    const image = req.files.file;
+
+    cloudinary.uploader.upload(image.tempFilePath, async (error, result) => {
+      if (error) {
+        return res.status(500).send(error);
+      }
+
+      const { title, summary, content } = req.body;
+      const postDoc = await Post.create({
+        title,
+        summary,
+        content,
+        cover: result.secure_url, 
+        author: info.id,
+      });
+      res.json(postDoc);
+    });
+  });
+});
+
+app.put('/post', async (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, jwtSecret, {}, async (err, info) => {
+    if (err) {
+      console.error('Token verification failed:', err);
+      return res.status(401).json({ error: 'Invalid token' });
+    }
+
     const { id, title, summary, content } = req.body;
     const postDoc = await Post.findById(id);
     const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
@@ -157,33 +166,53 @@ app.put('/post', uploadMiddleware.single('file'), async (req, res) => {
       return res.status(400).json('You are not the author of the post');
     }
 
-    const updatedPost = await Post.findByIdAndUpdate(
-      id,
-      {
-        title,
-        summary,
-        content,
-        cover: newPath ? newPath : postDoc.cover,
-      },
-      { new: true }
-    );
+    if (req.files && Object.keys(req.files).length > 0) {
+      const image = req.files.file;
 
-    res.json(updatedPost);
+      cloudinary.uploader.upload(image.tempFilePath, async (error, result) => {
+        if (error) {
+          return res.status(500).send(error);
+        }
+
+        const updatedPost = await Post.findByIdAndUpdate(
+          id,
+          {
+            title,
+            summary,
+            content,
+            cover: result.secure_url,
+          },
+          { new: true }
+        );
+
+        res.json(updatedPost);
+      });
+    } else {
+      const updatedPost = await Post.findByIdAndUpdate(
+        id,
+        {
+          title,
+          summary,
+          content,
+        },
+        { new: true }
+      );
+
+      res.json(updatedPost);
+    }
   });
 });
 
-
 app.get('/post', async (req, res) => {
-  const posts = await Post.find().populate('author').sort({createdAt:-1}).limit(12)
-  // console.log(posts)
-  res.json(posts)
-})
+  const posts = await Post.find().populate('author').sort({ createdAt: -1 }).limit(12);
+  res.json(posts);
+});
 
 app.get('/post/:id', async (req, res) => {
-  const {id} = req.params
-  const postDoc = await Post.findById(id).populate('author', ['username'])
-  res.json(postDoc)
-})
+  const { id } = req.params;
+  const postDoc = await Post.findById(id).populate('author', ['username']);
+  res.json(postDoc);
+});
 
 app.get('/', (req, res) => {
   res.send('MERN Blog API');
